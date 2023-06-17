54c1877f9a75072564ac650423aa9b2bcf444436
Jonathan Boan <jonboan@gmail.com>
2023-06-17 16:15:10 +0930

    added some documentation diagrams and links

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PHS
 PHS (or Python Height Sensor) is a repository where JonBoan And NoIDidntHackU will work together to make the code and schematics for the Digital height detector based on python on the Raspberry Pi Pico W
+
+
+![system_component](./doc/system_component_layout.drawio.png)
+
+
+Circuit Layout 
+
+![max7219_connect](./doc/schematic7sgmnt.png)
+
+## Setup 
+
+- Setup [Thonny](https://thonny.org/)
+- Install the firmware on Pico - TODO - find the reference
+- Plugin the Pico
+- Add the `max7219_8digit.py` to the Pico via `Files` in Thonnny
+
+
--- code.py
+++ code.py
@@ -1,35 +1,35 @@
-read from Distance sensor (Jonboan?):
+# read from Distance sensor (Jonboan?):
 
 
 
 
 
 
-************************************************
+# ************************************************
 
-write to 8 digit 7 segment display (NoIDidntHackU):
-Schematic:
-schematic7sgmnt.png
- (for some reason doesn't work, not too sure why. maybe you could check it?)
-********************REQUIRED********************
-MicroPython library for MAX7219 + 8 x 7digit display boards.
+# write to 8 digit 7 segment display (NoIDidntHackU):
+# Schematic:
+# schematic7sgmnt.png
+#  (for some reason doesn't work, not too sure why. maybe you could check it?)
+# ********************REQUIRED********************
+# MicroPython library for MAX7219 + 8 x 7digit display boards.
 
-Download the max7219_8digit.py library
+# Download the max7219_8digit.py library
 
-Open the RPi Pico as drive, and then copy the library into root directory.
+# Open the RPi Pico as drive, and then copy the library into root directory.
 
-The library provides
+# The library provides
 
--- Initializing the device
+# -- Initializing the device
 
--- Set intensity of the device
+# -- Set intensity of the device
 
--- Write To Buffer functionality
+# -- Write To Buffer functionality
 
--- Send the buffer to MAX7219 device for further processing.
-************************************************
+# -- Send the buffer to MAX7219 device for further processing.
+# ************************************************
 
-Example of use:
+# Example of use:
 
 # Connections:
 # SCK (CLK) -> GPIO4 (D2)
@@ -37,7 +37,8 @@ Example of use:
 # SS (CS) -> GPIO5 (D1)
 
 from machine import Pin, SPI
-import max7219_8digit
+#import max7219_8digit
+import max7219
 
 # the max7219 doesn't use the MISO, but unfortunately SoftSPI requires that
 # we specify it anyway
@@ -49,8 +50,8 @@ display = max7219_8digit.Display(spi, ss)
 display.write_to_buffer('12345678')
 display.display()
 
-************************************************
-There is also a write_to_buffer_with_dots method, which attempts to use the display decimal points properly. Note that there are some rather big bugs in this at the moment, do not put dots at the start of the string, or put two of them in a row.
+#************************************************
+#There is also a write_to_buffer_with_dots method, which attempts to use the display decimal points properly. Note that there are some rather big bugs in this at the moment, do not put dots at the start of the string, or put two of them in a row.
 
 # Connections:
 # SCK (CLK) -> GPIO4 (D2)
@@ -68,4 +69,4 @@ display = max7219_8digit.Display(spi, ss)
 display.write_to_buffer_with_dots('1234.56.78')
 display.display()
 
-************************************************
+#************************************************
